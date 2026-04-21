b2b2e98ef101320752b717f03695e23ad229cca8
auryspintodelgado-boop <aruryspintodelgado@gmail.com>
2026-04-21 12:25:13 -0400

    suve esto

--- app/Database/Migrations/2024-01-01-000013_CreateAuditLogsTable.php
+++ app/Database/Migrations/2024-01-01-000013_CreateAuditLogsTable.php
@@ -58,6 +58,10 @@ class CreateAuditLogsTable extends Migration
                 'type' => 'DATETIME',
                 'null' => true,
             ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
         ]);
 
         $this->forge->addKey('id', true);
